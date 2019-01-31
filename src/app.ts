5c56c2a5aa577ac4d7768187949b04a48dd4c481
Anil <anil@FVFWL2GMHV27.local>
2019-01-31 12:16:47 +0530

    fix

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 const MongoStore = mongo(session);
 
 // Load environment variables from .env file, where API keys and passwords are configured
-// dotenv.config({ path: ".env" });
+dotenv.config({ path: ".env" });
 
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
